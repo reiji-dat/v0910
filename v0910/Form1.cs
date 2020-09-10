9081d589ec699cd48c43611cb81b61a1907b8c25
A601-12 <mkii.dat@gmail.com>
2020-09-10 09:34:21 +0900

    第一弾

--- v0910/Form1.cs
+++ v0910/Form1.cs
@@ -12,9 +12,30 @@ namespace v0910
 {
     public partial class Form1 : Form
     {
+        int[] vx = new int[3];
+        int[] vy = new int[3];
+        Label[] labels = new Label[100]; 
+
+        private static Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
+            for (int i = 0;i<3;i++)
+            {
+                vx[i] = rand.Next(-15, 16);
+                vy[i] = rand.Next(-15, 16);
+                labels[i] = new Label();
+                labels[i].AutoSize = true;
+                labels[i].Text = "★";
+                Controls.Add(labels[i]);
+                labels[i].Left = rand.Next(ClientSize.Width - labels[i].Width);
+                labels[i].Top = rand.Next(ClientSize.Height - labels[i].Height);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
